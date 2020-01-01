5c65b960cc37e3ae52c43cf22056f7ff491efca1
Nathan <nathan.froeh@gmail.com>
2020-01-01 13:28:17 -0700

    Set base css

---
+++ src/Signin.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Typography from '@material-ui/core/Typography';
+
+export default function App() {
+  return (
+    <div className="Sign-in">
+      <Typography component="h1" variant="h5">
+        Sign in
+      </Typography>
+    </div>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Signin from './Signin';
+import Signin from './Signin.jsx';
+import User from './User.jsx';
 import * as serviceWorker from './serviceWorker';
 import { BrowserRouter, Route } from 'react-router-dom';
 
@@ -9,6 +10,7 @@ ReactDOM.render(
 
   <BrowserRouter>
     <Route path='/' component={Signin}/>
+    <Route path='/user' component={User}/>
   </BrowserRouter>
   
 , document.getElementById('root'));
