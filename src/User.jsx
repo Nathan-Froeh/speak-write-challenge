79e57d1ecf5040106eb4c13925be8c7e10b3409a
Nathan <nathan.froeh@gmail.com>
2020-01-01 18:28:19 -0700

    Center user info

--- src/User.jsx
+++ src/User.jsx
@@ -4,6 +4,8 @@ import { AppBar, Avatar } from '@material-ui/core';
 import { ThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
 import Toolbar from '@material-ui/core/Toolbar';
+import Box from '@material-ui/core/Box';
+import Grid from '@material-ui/core/Grid';
 
 const theme = createMuiTheme({
   palette: {
@@ -20,7 +22,7 @@ export default function User() {
   return (
     <div className='user-page'>
       <ThemeProvider theme={theme}>
-      <AppBar style={{background: '#282828'}}>
+      <AppBar style={{background: '#282828'}} position="static">
         <Toolbar className='nav'>
           <Typography component="h1" variant="h5">
             SpeakWrite Logo
@@ -34,7 +36,37 @@ export default function User() {
           </div>
         </Toolbar>
       </AppBar>
-      <p>{localStorage.getItem('username')}</p>
+      <Box component="section" className='welcome' p={1}>
+        <Typography component='h1' variant='h3'>
+          Welcome back!
+        </Typography>
+      </Box>
+      <Grid 
+      container
+      spacing={0}
+      direction="column"
+      alignItems="center"
+      alignContent="center"
+      justify="center"
+      >
+        <Box>
+          <Typography component='h2'>
+            Username: {localStorage.getItem('username')}
+          </Typography>
+          <Typography component='h2'>
+            Email: fake@fake.com
+          </Typography>
+          <Typography component='h2'>
+            Name: John Doe
+          </Typography>
+          <Typography component='h2'>
+            Address: 123 Mountain rd Lakewood Colorado, 80226
+          </Typography>
+          <Typography component='h2'>
+            Phone: 555-555-5555
+          </Typography>
+        </Box>
+      </Grid>
       </ThemeProvider>
     </div>
   )
